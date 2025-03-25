cfd760e4c77839868ac7af1d686bd13ce15059d3
jkmh-dev <jkmamanih.dev@gmail.com>
2025-03-24 19:23:33 -0500

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'astro',
+  base: '',
   vite: {
     plugins: [tailwindcss()]
   }
